0a3b891f4ac6e6e1bda0340fb8b96b175736e83e
li.zhaoguang <li.zhaoguang@immomo.com>
2020-08-11 14:04:12 +0800

    init

--- js/App.tsx
+++ js/App.tsx
@@ -5,15 +5,6 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import Constants from 'expo-constants';
-
-/**
- * 修复警告
- * Linking.makeUrl is not supported in bare workflow. Switch to using your scheme string directly.
- */
-// @ts-ignore
-Constants.manifest = { scheme:  "timepill" }
-console.log(Constants.manifest)
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
