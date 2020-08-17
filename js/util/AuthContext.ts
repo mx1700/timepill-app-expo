14a3d13d9e7867177c74881a79b17d799235b1a5
li.zhaoguang <li.zhaoguang@immomo.com>
2020-08-17 17:31:05 +0800

    fix

--- js/util/AuthContext.ts
+++ js/util/AuthContext.ts
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AuthContext = React.createContext({
     isLogin: false,
-    setLogin: (success: Boolean) => {},
+    setLogin: (success: boolean) => {},
 });
 
 export default AuthContext
